97d21dff317423e964bd09df10b76d606b5bce8c
Akshad7829 <73118552+Akshad7829@users.noreply.github.com>
2021-05-12 19:51:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import React, { useState } from 'react';
+import {EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import Text from 'react-text-typing';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import draftToHtml from 'draftjs-to-html';
+  import htmlToDraft from 'html-to-draftjs';
+import ReactFileReader from 'react-file-reader';
+import Save from './save.js'
+import './styles.css';
+const App = () => {
+
+  const [editorState, setEditorState] = useState();
+
+ const onEditorStateChange =() =>{
+   setEditorState.createEmpty();
+ };
+  return (
+    <div className="App">
+       <Text
+    text="Text-Editor"
+    showBlink={true}
+    component="h2"
+    className="App-header"
+  />
+
+
+
+      <Editor editorState={editorState}
+        onEditorStateChange={setEditorState}
+        toolbarClassName="toolbarClassName"
+         wrapperClassName="wrapperClassName"
+         editorClassName="editorClassName"
+         id="input"
+
+         placeholder=<em>Start typing from here....</em> />
+     
+
+           
+
+  <p className="foot"> Made With 🧡<br/> By Akshad Nayakwadi</p>
+  </div>
+);
+}
+export default App;
